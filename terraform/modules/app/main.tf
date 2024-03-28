8ee214d9262829af7fdb1c7285ceb5d40466af3e
mikhailbelikov <belikovmq@gmail.com>
2024-03-28 15:54:14 +0300

    Terraform 2 homework fix

---
+++ terraform/modules/app/main.tf
@@ -0,0 +1,65 @@
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#  required_version = ">= 0.13"
+#}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+resource "yandex_compute_instance" "app" {
+  count = var.vm_count
+  name = "reddit-app-${var.environment_space}-${count.index}"
+  zone = var.zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.app_disk_image
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${var.mongo_ip}"
+  }
+
+  provisioner "file" {
+    source      = "../modules/app/files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sed -i '/Restart*/a Environment=\"DATABASE_URL=${var.mongo_ip}\"' /tmp/puma.service"
+    ]
+  }
+  provisioner "remote-exec" {
+    script = "../modules/app/files/deploy.sh"
+  }
+}
